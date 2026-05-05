885fea4000158f53ae915eb92156df3538cf8e6b
raghav-latte <rsraghavsethi@gmail.com>
2026-05-05 22:03:22 +0530

    Initial Commit

---
+++ Wabi-Intro/Wabi_IntroApp.swift
@@ -0,0 +1,17 @@
+//
+//  Wabi_IntroApp.swift
+//  Wabi-Intro
+//
+//  Created by Raghav Sethi on 5/5/26.
+//
+
+import SwiftUI
+
+@main
+struct Wabi_IntroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
